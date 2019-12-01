5807215c1528d74972f90c2956bd0bee80a4332a
Dennis Su <dennis.su@verizonmedia.com>
2019-12-01 10:04:18 +0800

    Init project.

---
+++ src/main/java/pers/ssh/admin/console/AdminConsole.java
@@ -0,0 +1,16 @@
+package pers.ssh.admin.console;
+
+/**
+ * Author:   dsu01
+ * Date:     2019/12/1 9:54 上午
+ * Description: The entry point for this admin console.
+ */
+public class AdminConsole {
+
+    public static void main(String[] args) {
+        System.out.println("Starting...");
+
+
+
+    }
+}
